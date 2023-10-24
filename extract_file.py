6295eb069a568ecb1fcabc5c6bb9058f69c30175
omolevictor97 <omolevictor97@gmail.com>
2023-10-24 06:38:46 +0100

    Extraction file added

---
+++ extract_file.py
@@ -0,0 +1,20 @@
+from src.exception import CustomException
+import os, sys
+from src.logger import logging
+from zipfile import ZipFile
+
+
+def extract_folder(file_path:str, folder_path=None):
+    try:
+        with ZipFile(file_path, 'r') as zObject:
+            if folder_path is not None:
+                zObject.extractall(folder_path)
+            else:
+                zObject.extract()
+            return folder_path if folder_path else os.get_cwd()
+    except Exception as e:
+        logging.info("An error has occured")
+        raise CustomException(e, sys)
+    
+
+
